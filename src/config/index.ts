1f49c6476098c9495f9c8675be57a3d5f6621e31
amall-j <amonovamal562@gmail.com>
2025-10-04 19:48:50 +0500

    First commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const API_URL = import.meta.env.VITE_APP_API_URL;
 export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
-export const tokenName = "uzimeiToken";
+export const tokenName =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzYyYTAxNjhmNDA5ODAzZWVmM2Y2NjhmYTg0ZTllZSIsIm5iZiI6MTc1OTU3MTI4MS43NDQsInN1YiI6IjY4ZTBlZDUxMWU3ZjcyMDFiMjljYTIyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4G8XaGwyyZ_Ep7-jBPF_PRN-Rjht6U5KboZZaexcGOE";
 export const languageName = "uzimeiLanguage";
--- src/views/home/view.tsx
+++ src/views/home/view.tsx
@@ -1,5 +1,32 @@
 const Home = () => {
-  return <div>asd</div>;
+  return (
+    <div className="container-cinema  mx-auto bg-red">
+      <p className="font-qanelas  ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, culpa?
+      </p>
+      <p className="font-qanelas font-black ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, culpa?
+      </p>
+      <p className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, culpa?
+      </p>
+      <div className="blue_hover bg-blue">
+        {" "}
+        <p className="font-qanelas  ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          culpa?
+        </p>
+        <p className="font-qanelas font-black ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          culpa?
+        </p>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+          culpa?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
